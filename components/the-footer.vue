0927e489f03be167927427b2e1b0864df0122f52
J <>
2024-01-16 17:26:45 +0800

    feat: update footer

--- components/the-footer.vue
+++ components/the-footer.vue
@@ -1,12 +1,17 @@
 <template>
 	<footer
-		class="flex h-screen w-screen flex-col items-center justify-center gap-10 p-10"
+		class="flex h-screen w-screen flex-col items-center justify-center gap-5 p-10"
 	>
 		<h1 class="font-Tangerine text-7xl font-bold text-brand">Thank you</h1>
 
-		<p class="text-center text-xs leading-relaxed text-gray-400">
-			This website is not affiliated with the <b>Oscars®</b>. <br />
-			Author:
+		<p class="text-center text-xs leading-relaxed opacity-75">
+			<a
+				href="https://github.com/jissinl/oscars.site"
+				target="_blank"
+				class="font-bold underline"
+				>MIT license.</a
+			>
+			Made with ♥ by
 			<a href="https://x.com/0hxlu" target="_blank" class="font-bold underline"
 				>lu(x.com/0hxlu)</a
 			>
